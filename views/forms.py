dba91fc11abbc7f8b12ed52fbe05d4ea9b9ffa03
caustinbakker <caustinbakker@gmail.com>
2018-03-31 09:19:22 +0200

    added create post page with test form

---
+++ views/forms.py
@@ -0,0 +1,12 @@
+"""Template for all forms on the website."""
+from flask_wtf import FlaskForm
+from wtforms.validators import DataRequired, ValidationError
+from wtforms import DateField, StringField
+
+
+class Create_post(FlaskForm):
+    """Add planning to database."""
+
+    Title = StringField('Title',
+                        validators=[DataRequired()]
+                        )
--- views/views.py
+++ views/views.py
@@ -3,6 +3,8 @@ from __main__ import app
 from database import models
 from flask import Flask, render_template
 
+from views import forms
+
 
 @app.route('/', methods=('GET', 'POST'))
 def main():
@@ -15,3 +17,10 @@ def category(category):
     """Display posts from category."""
     return render_template('category.html', category=models.Category.
                            get(models.Category.name == category))
+
+
+@app.route('/create_post')
+def create_post():
+    """Create a blog post."""
+    form = forms.Create_post()
+    return render_template('create_post.html', form=form)
