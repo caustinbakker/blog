7dcdee764fdd15f251f4dd4ad3b6187403ee8275
caustinbakker <caustinbakker@gmail.com>
2018-04-24 17:55:05 +0200

    added config file

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
-"""Config file for flask app."""
+"""Config file."""
 DEBUG = True
 TESTING = False
-SECRET_KEY = '1243165fg78h2415634f35fdf89hg489hjf9092j23jjf9928fisd021j90j'
 TEMPLATES_AUTO_RELOAD = True
 UPLOADED_PHOTOS_DEST = 'static/'
+SECRET_KEY = '12341231231231223153254redsc234resdf4f34'
