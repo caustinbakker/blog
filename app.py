b0c623dd7ae618cb4081cfc4afe933b5fbfe67cb
caustinbakker <caustinbakker@gmail.com>
2018-05-04 12:54:07 +0200

    changed image fize to 1024px

--- app.py
+++ app.py
@@ -3,13 +3,14 @@ from flask import Flask
 from peewee import *
 from livereload import Server, shell
 from flask_uploads import UploadSet, IMAGES, configure_uploads
+from io import StringIO
+
 
 import models
 
 app = Flask(__name__)
 app.config.from_pyfile('config.py', silent=True)
 DEBUG = app.debug
-
 
 photos = UploadSet('photos', IMAGES)
 configure_uploads(app, photos)
